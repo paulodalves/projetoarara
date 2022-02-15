2f17b8840e77c6ea621fc1bb09233257c5e67e79
paulodalves <paulodalves95@gmail.com>
2022-02-15 19:38:42 -0300

    versão entrega

--- src/main/webapp/script/main.js
+++ src/main/webapp/script/main.js
@@ -1,4 +1,4 @@
-const apiKey = 'pk.eyJ1IjoicGF1bG9kYWx2ZXMiLCJhIjoiY2t6bmwwY3B3MGxvZjJvbWdzNm1hbHZ3byJ9.ex5YK9jYvrs-BEm32uNlZQ';
+const apiKey = 'pk.eyJ1IjoicGF1bG9kYWx2ZXMiLCJhIjoiY2t6b2tyaXlyMmR2aTJ3czhsMWtnZjdxdSJ9.Pg6a_mC_8xkjq96bH2NMFw';
 
 const map = L.map('map').setView([-10.532681, -54.552170], 4);
 
@@ -11,61 +11,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: apiKey
 }).addTo(map);
 
+
 // Adiciona marcação
-const marker1 = L.marker([-7.578922, -54.431689]).addTo(map);
-const marker2 = L.marker([-12.807879, -57.844931]).addTo(map);
-const marker3 = L.marker([-5.715164, -62.929243]).addTo(map);
-const marker4 = L.marker([-18.560731, -52.823945]).addTo(map);
-const marker5 = L.marker([-10.398466, -50.012593]).addTo(map);
+var marker = L.marker([-23.658263, -46.648904]).addTo(map);
+var marker = L.marker([-7.811690, -36.471794]).addTo(map);
+var marker = L.marker([-3.068994, -60.042304]).addTo(map);
 
-// mensagem
 
-let tamplate1 = `
-	<h3>Nome da Aldeia</h3>
-	<p>Contato<p>
-	<div style="text-align: center;">
-		<img width="150px" heigth="150px" src="imagens/imagem01.jpg" />
-	</div>
-`;
-
-marker1.bindPopup(tamplate1);
-
-let tamplate2 = `
-	<h3>Nome da Aldeia</h3>
-	<p>Contato<p>
-	<div style="text-align: center;">
-		<img width="150px" heigth="150px" src="imagens/imagem01.jpg" />
-	</div>
-`;
-
-marker1.bindPopup(tamplate2);
-
-let tamplate3 = `
-	<h3>Nome da Aldeia</h3>
-	<p>Contato<p>
-	<div style="text-align: center;">
-		<img width="150px" heigth="150px" src="imagens/imagem01.jpg" />
-	</div>
-`;
-
-marker1.bindPopup(tamplate3);
-
-let tamplate4 = `
-	<h3>Nome da Aldeia</h3>
-	<p>Contato<p>
-	<div style="text-align: center;">
-		<img width="150px" heigth="150px" src="imagens/imagem01.jpg" />
-	</div>
-`;
-
-marker1.bindPopup(tamplate4);
-
-let tamplate5 = `
-	<h3>Nome da Aldeia</h3>
-	<p>Contato<p>
-	<div style="text-align: center;">
-		<img width="150px" heigth="150px" src="imagens/imagem01.jpg" />
-	</div>
-`;
-
-marker1.bindPopup(tamplate5);
